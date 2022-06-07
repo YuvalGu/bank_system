5a1d0455718198542b1c0d35c6ef29fa2daef796
YuvalGu <yuval5659@gmail.com>
2022-06-07 19:31:36 +0300

    Costumer.py - done with notes

--- Costumer.py
+++ Costumer.py
@@ -1,17 +1,13 @@
 class Costumer:
     def __init__(self, id: str, name: str, pin_code: str):
+        """
+        :param id: costumer id
+        :param name: costumer's name
+        :param pin_code: the costumer's password
+        """
         self.id = id
         self.name = name
         self.password = pin_code
-        # self.accounts_id = []
-
-    # def add_account(self, account_id):
-    #     self.accounts_id.append(account_id)
-    #
-    # def close_account(self, account_id):
-    #     self.accounts_id.remove(account_id)
 
     def __eq__(self, other):
         return self.id == other.id
-
-
